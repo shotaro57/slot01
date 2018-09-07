76792e573a587ebd7c5e79f742b7a1a0a8cb3e62
takotakotako33 <euro11kuro@yahoo.co.jp>
2018-09-07 22:45:18 +0900

    乱数発生させました

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -9,14 +9,16 @@ public class Main : MonoBehaviour {
 	public static bool isReelLeftStop = false;
 	public static bool isReelRightStop = false;
 	public static string role = "はずれ";
+	public static int settei;
 
 	// Use this for initialization
 	void Start () {
-				
-	}
+        settei = Random.Range(1, 7);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
 }
