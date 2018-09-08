4868e88a31562ba4b805f3f89f0a7c21b92272fa
takotakotako33 <euro11kuro@yahoo.co.jp>
2018-09-08 16:43:41 +0900

    Performance.csをつくった

--- Assets/Scripts/Performance.cs
+++ Assets/Scripts/Performance.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Performance : MonoBehaviour {
-
+	private int peka;
+	private int pekakanri = 0;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +12,25 @@ public class Performance : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Main.jyunbiFlag){
+			if(!Main.Lump){
+				if(pekakanri == 0){
+					peka = Random.Range(0,4);
+					pekakanri=1;
+				}
+				if(peka == 0){
+							Main.Lump = true;
+							Debug.Log("先点灯中");
+							pekakanri=0;
+				}else{
+                    if(Main.playFlag == false){
+							Main.Lump = true;
+							Debug.Log("後点灯中");
+							pekakanri=0;
+					}
+					
+				}
+			}	
+		}
 	}
 }
